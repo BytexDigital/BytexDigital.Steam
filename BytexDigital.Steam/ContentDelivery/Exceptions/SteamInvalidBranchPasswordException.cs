cd72dc71ac700d0ebbbc5ec408365067e1d61fdd
Ryan Torzynski <ryantorzynski@gmail.com>
2020-05-10 04:37:13 +0200

    Fixed minor spelling mistake

--- BytexDigital.Steam/ContentDelivery/Exceptions/SteamInvalidBranchPasswordException.cs
+++ BytexDigital.Steam/ContentDelivery/Exceptions/SteamInvalidBranchPasswordException.cs
@@ -7,7 +7,7 @@ namespace BytexDigital.Steam.ContentDelivery.Exceptions
     public class SteamInvalidBranchPasswordException : Exception
     {
         public SteamInvalidBranchPasswordException(AppId appId, DepotId depotId, string branch, string password, Exception innerException = null) : base($"Invalid password = " +
-            $"'{password}' for branch = {branch} of app id app id = {appId}, depot id = {depotId}{(innerException != null ? ". View the inner exception for more details." : "")}", innerException)
+            $"'{password}' for branch = {branch} of app id = {appId}, depot id = {depotId}{(innerException != null ? ". View the inner exception for more details." : "")}", innerException)
         {
         }
     }
